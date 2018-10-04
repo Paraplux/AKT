d9b64bc012dc937433a60d4ea9d17fe8a6a0bb69
Marc Bouchez <theparaplux@gmail.com>
2018-10-04 09:34:08 +0200

    Big Update
    Multiple creation / update and bug fix

---
+++ components/navbar.php
@@ -0,0 +1,15 @@
+<div class='nav-fix'></div>
+<div id="navigation">
+    <div class="left-nav">
+        <img class="logo" src="../images/logo-final.svg" alt="">
+    </div>
+    <div id="nav-separator"></div>
+    <div class="right-nav">
+        <span><a href="#">HOME</a></span>
+        <span><a href="#">CRAFTMANSHIP</a></span>
+        <span><a href="#">COLLECTION</a></span>
+        <span><a href="#">NEWS</a></span>
+        <span><a href="#" id="purple">STORE</a></span>
+        <span><a href="#">ABOUT</a></span>
+    </div>
+</div>
---
+++ views/home.php
@@ -0,0 +1,18 @@
+<?php 
+include '../components/header.php';
+include '../components/navbar.php';
+?>
+<link rel="stylesheet" href="../css/welcome.css">
+
+
+<div id="animation">
+    <span id="a">A</span>
+    <span id="k">K</span>
+    <span id="t">T</span>
+    <span id="jewels">JEWELS</span>
+</div>
+
+<script src="../js/welcome.js"></script>
+<?php
+include '../components/footer.php';
+?>
