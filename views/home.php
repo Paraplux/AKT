879469c892cf728dae7c468d11f632a529374e7f
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-10-08 08:52:51 +0200

    Accueil
    Ajout de carrousel, mise en page et texte défilant

--- views/home.php
+++ views/home.php
@@ -11,19 +11,24 @@ include '../components/navbar.php';
     <span id="t">T</span>
     <span id="jewels">JEWELS</span>
 </div>
-
+    <div class="home-page">
+    <marquee scrollamount="8">ATTENTION : Event je sais plus quand sur les bijoux je sais plus où... </marquee>
 <div class="presentation welcomeHide">
     <div class="presentation-content">
-        <h1>titre</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est rerum tenetur consequuntur dolor consectetur sed laborum facilis voluptatem architecto deserunt ipsa magnam repellendus, reiciendis officia quibusdam harum doloribus eum velit!</p>
+        <h1 class="home-h1">François Ghys</h1>
+        <p>UN REVE D'ENFANT<br>
+        « J’ai toujours rêvé de fabriquer de beaux objets. Des objets au design affirmé, que l’on aime et que l’on garde. Des objets qui ont une histoire et qui expriment des valeurs.<br>
+        Il y a 2 ans, j’ai décidé de transformer ce rêve en réalité : c’est ainsi que l’aventure AKT a commencé »<br>
+        
+        François Ghys, fondateur de AKT Jewels.</p>
     </div>
     <div class="presentation-thumb"> 
-        <img src="../images/Francois-Ghys.jpg" alt="">
+                <img class="main-picture" src="../images/Francois-Ghys.jpg" alt="">
     </div>
 </div>
 
 <div class="flagship welcomeHide">
-    <h2 class="flagship-title">Les plus vendus</h2>
+    <h2 class="home-h2">Les plus vendus</h2>
     
     <div class="main-gallery js-flickity"
             data-flickity-options='{ "cellAlign": "left", "contain": true }'>
@@ -43,6 +48,12 @@ include '../components/navbar.php';
         <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
         <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
         <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
+        <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
+        <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
+        <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
+        <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
+        <div class="gallery-cell"><img src="../images/Francois-Ghys.jpg" alt=""></div>
+ 
     </div>
 
 </div>
@@ -53,3 +64,4 @@ include '../components/navbar.php';
 <?php
 include '../components/footer.php';
 ?>
+<div>
