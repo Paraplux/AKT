d1eba5439544a023dc4f64c835933f4bc70bc6c4
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-10-08 11:47:50 +0200

    Barre de défilement
    Barre de défilement

--- views/home.php
+++ views/home.php
@@ -11,23 +11,24 @@ include '../components/navbar.php';
     <span id="t">T</span>
     <span id="jewels">JEWELS</span>
 </div>
-    <div class="home-page">
-    <marquee scrollamount="8">ATTENTION : Event je sais plus quand sur les bijoux je sais plus où... </marquee>
-<div class="presentation welcomeHide">
-    <div class="presentation-content">
-        <h1 class="home-h1">François Ghys</h1>
-        <p>UN REVE D'ENFANT<br>
+
+<div class="home-page">
+    <div class="defil-container welcomeHide"><div class="defil">La Comté - FRANCOIS - FRANCOIS - FRANCOIS - FRANCOIS - FRANCOIS</div></div>
+        <div class="presentation welcomeHide">
+            <div class="presentation-content">
+                <h1 class="home-h1">François Ghys</h1>
+                <p class="title-page">UN REVE D'ENFANT<br>
         « J’ai toujours rêvé de fabriquer de beaux objets. Des objets au design affirmé, que l’on aime et que l’on garde. Des objets qui ont une histoire et qui expriment des valeurs.<br>
         Il y a 2 ans, j’ai décidé de transformer ce rêve en réalité : c’est ainsi que l’aventure AKT a commencé »<br>
         
         François Ghys, fondateur de AKT Jewels.</p>
-    </div>
-    <div class="presentation-thumb"> 
+            </div>
+            <div class="presentation-thumb"> 
                 <img class="main-picture" src="../images/Francois-Ghys.jpg" alt="">
-    </div>
-</div>
+            </div>
+        </div>
 
-<div class="flagship welcomeHide">
+    <div class="flagship welcomeHide">
     <h2 class="home-h2">Les plus vendus</h2>
     
     <div class="main-gallery js-flickity"
