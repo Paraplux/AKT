f36df52166e903bfe05ab055b4ca9ff9848fa650
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-10-11 15:57:49 +0200

    Debut Store
    Le commencement

---
+++ components/navbar-reverse.php
@@ -0,0 +1,16 @@
+<div class='nav-fix'></div>
+<div id="navigation" class="welcomeHide">
+    <div class="left-nav">
+        <img class="logo" src="../images/logo-final-reverse.svg" alt="">
+    </div>
+    <div id="nav-separator"></div>
+    <div class="right-nav">
+        <span><a href="../views/home.php">HOME</a></span>
+        <span><a href="../views/craft.html">CRAFTMANSHIP</a></span>
+        <span><a href="../views/collection.php">COLLECTION</a></span>
+        <span><a href="../views/news.php">NEWS</a></span>
+        <span><a href="../views/store.php" id="purple">STORE</a></span>
+        <span><a href="../views/about.php">ABOUT</a></span>
+        <span><i class="fab fa-instagram"></i><i class="fab fa-pinterest-p"></i></span>
+    </div>
+</div>
--- views/store.php
+++ views/store.php
@@ -1,9 +1,27 @@
 <?php 
 include '../components/header.php';
-include '../components/navbar.php';
+include '../components/navbar-reverse.php';
 ?>
 
+<link rel="stylesheet" href="../css/navbar-reverse.css">
+<link rel="stylesheet" href="../css/store.css">
 
+<style>
+body{
+    background-color : #ededed
+}
+</style>
+
+<div class="grid">
+                    <figure class="effect-apollo">
+                        <img src="../images/jewelry/brace-1.jpg" alt="img18"/>
+                        <figcaption>
+                            <h2>Bracelet <span>Of DOOM</span></h2>
+                            <p>Le bracelet de l'apocalypse</p>
+                            <a href="#">View more</a>
+                        </figcaption>            
+                    </figure>
+                </div>
 
 <?php
 include '../components/footer.php';
