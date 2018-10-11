89cf93cbc6f7f66575e576ddab277319df2984c1
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-10-11 16:20:34 +0200

    Categorie
    ajouts des catégories a la page store

--- views/store.php
+++ views/store.php
@@ -21,6 +21,30 @@ body{
                             <a href="#">View more</a>
                         </figcaption>            
                     </figure>
+                <figure class="effect-apollo">
+                        <img src="../images/jewelry/brace-2.jpg" alt="img18"/>
+                        <figcaption>
+                            <h2>Bracelet <span>Of DOOM</span></h2>
+                            <p>Le bracelet de l'apocalypse</p>
+                            <a href="#">View more</a>
+                        </figcaption>            
+                    </figure>
+                <figure class="effect-apollo">
+                        <img src="../images/jewelry/brace-3.jpg" alt="img18"/>
+                        <figcaption>
+                            <h2>Bracelet <span>Of DOOM</span></h2>
+                            <p>Le bracelet de l'apocalypse</p>
+                            <a href="#">View more</a>
+                        </figcaption>            
+                    </figure>
+                <figure class="effect-apollo">
+                        <img src="../images/jewelry/brace-4.jpg" alt="img18"/>
+                        <figcaption>
+                            <h2>Bracelet <span>Of DOOM</span></h2>
+                            <p>Le bracelet de l'apocalypse</p>
+                            <a href="#">View more</a>
+                        </figcaption>            
+                    </figure>
                 </div>
 
 <?php
