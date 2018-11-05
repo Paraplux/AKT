0ddaa729b6efdcc4e8d5a0e894a173c8bf6e5c53
Marc Bouchez <theparaplux@gmail.com>
2018-11-05 12:45:21 +0100

    Page item
    AJout css page item + fix angle sur le store

---
+++ views/item.php
@@ -0,0 +1,41 @@
+<?php 
+include '../components/header.php';
+include '../components/navbar-reverse.php';
+?>
+
+<link rel="stylesheet" href="../css/navbar-reverse.css">
+<link rel="stylesheet" href="../css/item.css">
+
+<div class="container">
+    <div class="product">
+        <div class="product-thumb">
+            <img src="../images/jewelry/brace-3.jpg" alt="">
+        </div>
+        <div class="product-cart">
+            <div class="product-title">
+                <h1>Bracelet</h1>
+                <h1>99€</h1>
+            </div>
+            <div class="product-variation">
+                <select>
+                    <option>Bleu</option> 
+                    <option>Marron</option>
+                    <option>Beige</option>
+                </select>
+            </div>
+            <div class="product-desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae voluptate itaque hic modi perferendis quo est, ipsa sit repudiandae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae voluptate itaque hic modi perferendis quo est, ipsa sit repudiandae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae voluptate itaque hic modi perferendis quo est, ipsa sit repudiandae.
+            </div>
+            <div class="product-buy">
+                <h2>Add to cart</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="../js/flickity.js"></script>
+
+<?php
+include '../components/footer.php';
+?>
