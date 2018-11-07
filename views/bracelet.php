59a272bce69249e11d28a1e694f4ae9e5b7cd775
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-11-07 15:20:08 +0100

    bracelet part1

---
+++ views/bracelet.php
@@ -0,0 +1,107 @@
+<?php 
+include '../components/header.php';
+include '../components/navbar-reverse.php';
+?>
+
+<link rel="stylesheet" href="../css/navbar-reverse.css">
+<link rel="stylesheet" href="../css/store.css">
+
+<div class="item-cat-navigation">
+    <span class="item-cat-links">Necklaces</span>
+    <span class="item-cat-links">Rings</span>
+    <span class="item-cat-links">Other</span>
+</div>
+
+<div class="item-cat-presentation">
+        <div class="item-cat-presentation-title"><h1>LE BRACELET</h1></div>
+        <div class="item-cat-presentation-thumb">
+            <img class="main-bracelet" src="../images/jewelry/brace-2.jpg" alt="">
+        </div>
+        <div class="item-cat-presentation-content"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae quo earum accusantium. Explicabo vero repudiandae, deserunt earum cumque maxime voluptatem nesciunt enim asperiores eaque possimus quibusdam, incidunt dolore sit atque?</p></div>
+</div>
+<div class="item-cat-mosaic">
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°1</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°2</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°3</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°4</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°5</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°6</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°7</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°8</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°9</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+    <div class="item-tile">
+        <div class="item-tile-title"><h3>bracelet n°10</h3></div>
+        <div class="item-tile-content">test</div>
+        <div class="item-tile-thumb">
+            <img class="bracelet" src="../images/jewelry/brace-1.jpg" alt="">
+        </div>
+        <div class="item-tile-price">666€</div>
+    </div>
+</div>
+
+<?php
+include '../components/footer.php';
+?>
