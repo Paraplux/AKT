2a1640fa010e4c8f7ebba3a384bc1ff6fe8d2d81
Marc Bouchez <theparaplux@gmail.com>
2018-11-08 14:50:03 +0100

    Ajout fichier DB

---
+++ components/db.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "marcboucvd0632.mysql.db";
+$bd = "marcboucvd0632";
+$user = "marcboucvd0632";
+$password = "Kmle0632";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$bd", $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+} catch (Exception $e) {
+    die("Erreur : " . $e->getMessage());
+}
