c92c4baa11fa0654e996549a39290e92bc9ca906
Marc Bouchez <theparaplux@gmail.com>
2018-11-19 14:52:35 +0100

    Petit fix

--- controllers/controller-store.php
+++ controllers/controller-store.php
@@ -3,5 +3,5 @@ include '../components/db.php';
 
     $req = $pdo->prepare('SELECT cat,thumb_1 FROM akt_store WHERE is_thumb = "true"');
     $req->execute();
-
     $storeDatas = $req->fetchAll();
+    $req->closeCursor();
