1786fd97711821b652507621a40f40f987133265
Marc Bouchez <theparaplux@gmail.com>
2018-11-28 12:54:04 +0100

    [TOGETHER FOREVER] CSS du ticket + php

--- views/ticket.php
+++ views/ticket.php
@@ -1,24 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../css/ticket.css">
-  <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
-</head>
-<body>
-  <div class="ticket">
-  <p class="title">AKT JEWELS</p>
-  <p class="slogan">ENTRER ICI LE SLOGAN</p>
-  <p class="date">date</p>
-  <p class="produit">PRODUIT</p>
-  <p>-----------------------------------------</p> 
-  <p class="price">TOTAL TTC: PRIX</p>
-  <p>*****************************************</p>
-  <p class="thank">INSERER MESSAGE DE REMERCIEMENT </p>
-  <p>*****************************************</p>
+<?php include '../components/header.php';
+include '../components/navbar.php';
+include '../controllers/controller-cart.php';
+?>
+
+<link rel="stylesheet" href="../css/ticket.css">
+  <div class="ticket-container">
+    <div class="ticket">
+      <p class="title">AKT JEWELS</p>
+      <p class="slogan">Creating mo' with ammo</p>
+      <p class="date">21 - 12 - 2018</p>
+      <?php
+      if (isset($_SESSION['cart'])) :
+        foreach ($itemsCart as $item) :
+      ?>
+        <a href="./item?ref=<?= $item['ref'] . "&color=" . $item['color_format']; ?>"><?= $item['name']; ?> (<?= $item['color']; ?>) - Ref :  <?= $item['ref'] ?></a>
+        <div>Qty : <?= $_SESSION['cart'][$item['id']] ?> x <strong><?= $item['prix']; ?> €</strong></div>
+        <hr>
+        <?php
+        endforeach;
+        ?>
+        <p>-----------------------------------------</p> 
+        <p><strong>TOTAL TTC : <?= $totalPrice ?> $</strong></p>
+    <?php
+    else :
+    ?>
+    <p><strong>Votre panier est vide!</strong></p>
+    <?php 
+    endif;
+    ?>
+      <p>*****************************************</p>
+      <p class="thank">AKT Jewels thanks you !</p>
+      <p>*****************************************</p>
+    </div>
   </div>
-</body>
-</html>
+  <div class="payement-button-container">
+    <div class="payement-button">
+      PAY
+    </div>
+  </div>
+
+<?php
+include '../components/footer.php';
+?>
