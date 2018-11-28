2e0c7c2bcc23e3b96a405a58268926d876b0d4bb
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-11-28 11:50:59 +0100

    Ticket Proto

---
+++ views/ticket.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Page Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/ticket.css">
+  <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+</head>
+<body>
+  <div class="ticket">
+  <p class="title">AKT JEWELS</p>
+  <p class="slogan">ENTRER ICI LE SLOGAN</p>
+  <p class="date">date</p>
+  <p class="produit">PRODUIT</p>
+  <p>-----------------------------------------</p> 
+  <p class="price">TOTAL TTC: PRIX</p>
+  <p>*****************************************</p>
+  <p class="thank">INSERER MESSAGE DE REMERCIEMENT </p>
+  <p>*****************************************</p>
+  </div>
+</body>
+</html>
