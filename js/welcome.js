6ef9f0360d05d9654017cf4a953c6ea65367dcf0
Marc Bouchez <theparaplux@gmail.com>
2018-11-30 10:27:52 +0100

    Cookie newsletter

--- js/welcome.js
+++ js/welcome.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    // if (localStorage.getItem('rgpdState') != 'shown') {
-    //     $('.welcome-popup').css('display', 'flex');
-    //     localStorage.setItem('rgpdState', 'shown');
-    // }
+    if (localStorage.getItem('rgpdState') != 'shown') {
+        $('.welcome-popup').css('display', 'flex');
+        localStorage.setItem('rgpdState', 'shown');
+    }
     $('.dismiss').on('click', function(){
         $('.welcome-popup').fadeOut('slow');
     });
